44758067cb4b4b53635da863a00a803290fb4fdc
Ida Beltoft <idalyngsoe03@gmail.com>
2024-09-18 13:01:15 +0200

    Created a epoch program

---
+++ epoch/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int seconds = 1726531205;
+int oneyear = 31556926;
+int years = seconds / oneyear;
+int rest = seconds % oneyear;
+int oneday = 86400;
+int days = rest / oneday;
+
+Console.WriteLine(years);
+Console.WriteLine(rest);
+Console.WriteLine(years + " " + days);
